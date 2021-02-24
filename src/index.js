653f1ae25afcd935f8dbc1e5f5462b4e95ebccf1
George Zhou <george.zhou@capco.com>
2021-02-24 12:13:45 -0500

    Enhance runtime transform to avoid duplication
    npm install --save-dev @babel/plugin-transform-runtime
    npm install --save @babel/runtime
    npm install --save @babel/runtime-corejs3
    
    Also reduce global pollutation for all new polyfill function
    definitions
    
    when having both presets and plugins configured, the orders is
    to run plugins first in defined orders, for preset, it runs in
    reversed order, i.e. stack style

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const isHas = [1, 2, 3].includes(2);
 const p = new Promise((resolve, reject) => {
     resolve(100);
 })
+
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    getX() {
+        return this.x;
+    }
+}
+
+let cp = new Point(25, 8)
