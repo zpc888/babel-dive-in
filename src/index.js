cb0ae85181e63ec75c5619f875ca8b85f09f71de
George Zhou <george.zhou@capco.com>
2021-02-24 10:08:31 -0500

    Use babel polyfill, but it uses the whole polyfill
    npm install --save @babel/polyfill
    Some functions or classes donot exist in older browser, to polyfill
    these @babel/polyfill is provided. It must be imported at the
    beginning of index.js However import everything will cause about 89k,
    out of which our code may just use 300 bytes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const fn = () => {
-    console.log('hello world');
-};
-fn();
+import '@babel/polyfill'
+
+const isHas = [1, 2, 3].includes(2);
+
+const p = new Promise((resolve, reject) => {
+    resolve(100);
+})
